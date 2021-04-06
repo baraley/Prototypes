b442a5bca44857810d4101397af73a6c4624b0dc
Alexander Baraley <alexander.baraley@gmail.com>
2021-04-06 09:32:54 +0300

    Initial Commit

---
+++ Prototypes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototypes
+//
+//  Created by Alexander Baraley on 06.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
